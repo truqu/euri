c9c78324979243c3f034b8ce594b5f725b5ea253
Michel Rijnders <michel.rijnders@truqu.com>
2017-08-03 10:40:23 +0200

    Add `euri:new/0` and `euri:new/1` (WIP)

--- src/euri.erl
+++ src/euri.erl
@@ -1,11 +1,49 @@
 -module(euri).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% API exports
--export([]).
+-export([ new/0
+        , new/1
+        ]
+       ).
+
+%% Record and macro definitions
+-record( uri
+       , { scheme = "https"   :: nonempty_string()
+         , host = "localhost" :: nonempty_string()
+         , port = 80          :: non_neg_integer()
+         , path = ""          :: string()
+         }
+       ).
 
 %%%-----------------------------------------------------------------------------
 %%% API functions
 %%%-----------------------------------------------------------------------------
+
+new() ->
+  new(#{}).
+
+new(Args) ->
+  #uri{}.
+
+%%%-----------------------------------------------------------------------------
+%%% Tests
+%%%-----------------------------------------------------------------------------
+
+-ifdef(TEST).
+
+new_test() ->
+  %% Test defaults
+  U = new(),
+  "https" = U#uri.scheme,
+  "localhost" = U#uri.host,
+  80 = U#uri.port,
+  "" = U#uri.path.
+
+-endif.
 
 %% Local variables:
 %% mode: erlang
